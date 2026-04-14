89be12a89e2621de7ac54986441c51e20871c197
fabis1ak1 <fabiansiema@gmail.com>
2026-04-15 00:39:59 +0200

    Klasa Reservation.cs

---
+++ WebApplication1/Models/Reservation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    
+    [Required]
+    public int RoomId { get; set; }
+    
+    [Required(ErrorMessage = "Nazwa organizatora jest wymagana.")]
+    public string OrganizerName { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Temat jest wymagany.")]
+    public string Topic { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime Date { get; set; }
+
+    [Required]
+    public TimeSpan StartTime { get; set; }
+
+    [Required]
+    public TimeSpan EndTime { get; set; }
+
+    public string Status { get; set; } = "planned";
+}
